42902c2d605fe6d76a400e1f5bf7d5860b8dc956
botfigther02 <105109331+botfigther02@users.noreply.github.com>
2023-07-30 02:25:39 +0600

    Frame, Border, Paddle, Ball, Collision

--- src/BlockBreaker/Gameplay.java
+++ src/BlockBreaker/Gameplay.java
@@ -1,17 +1,19 @@
 package BlockBreaker;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import javax.swing.Timer;
 
 public class Gameplay extends JPanel implements KeyListener, ActionListener {
     private boolean play= false;
     private int score= 0;
     private int totalBricks= 21;
 
-    private Timer time;
+    private Timer timer;
 
     private int delay= 8;
 
@@ -29,26 +31,85 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
-        time= new Timer(delay, this);
-        time.start();
+        timer= new Timer(delay, this);
+        timer.start();
+    }
+
+    public void paint(Graphics g){
+        g.setColor(Color.black);
+        g.fillRect(1, 1, 692, 592);
+        g.setColor(Color.yellow);
+        g.fillRect(0, 0, 3, 592);
+        g.fillRect(0, 0, 692, 3);
+        g.fillRect(691, 0, 3, 592);
+
+        g.setColor(Color.green);
+        g.fillRect(playerX, 550, 100, 8);
+
+        g.setColor(Color.yellow);
+        g.fillOval(ballPosX, ballPosY, 20, 20);
+        g.dispose();
+
     }
     @Override
     public void actionPerformed(ActionEvent e) {
+        timer.start();
 
+        if(play){
+            if(new Rectangle(ballPosX, ballPosY, 20, 20).intersects(new Rectangle(playerX, 550, 100, 8))){
+                ballYdir= -ballYdir;
+            }
+            ballPosX+= ballXdir;
+            ballPosY+= ballYdir;
+
+            if(ballPosX<0){
+                ballXdir= -ballXdir;
+            }
+            if(ballPosY<0){
+                ballYdir= -ballYdir;
+            }
+            if(ballPosX>670){
+                ballXdir= -ballXdir;
+            }
+
+        }
+        repaint();
     }
 
     @Override
-    public void keyTyped(KeyEvent e) {
+    public void keyTyped(KeyEvent e) {}
 
-    }
+    @Override
+    public void keyReleased(KeyEvent e) {}
 
     @Override
     public void keyPressed(KeyEvent e) {
-
+        if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+            if(playerX>=600){
+                playerX= 600;
+            }
+            else{
+                moveright();
+            }
+        }
+        if(e.getKeyCode() == KeyEvent.VK_LEFT){
+            if(playerX<10){
+                playerX= 10;
+            }
+            else{
+                moveleft();
+            }
+        }
     }
 
-    @Override
-    public void keyReleased(KeyEvent e) {
-
+    public void moveright(){
+        play= true;
+        playerX+=20;
     }
+    public void moveleft(){
+        play= true;
+        playerX-=20;
+    }
+
+
 }
